a096ca6e40d5cc5ff2e3ff8cc38ea937ffab8961
medakin <dm.medakin.online@gmail.com>
2025-01-08 17:59:35 +0400

    add workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     `maven-publish`
     `java-platform`
     `version-catalog`
+    signing
 }
 
 allprojects {
-    group = "io.zenhelix.platform"
+    group = "io.github.zenhelix"
 }
 
 subprojects {
@@ -13,6 +14,13 @@ subprojects {
 
     publishing {
         repositories {
+            maven("https://central.sonatype.com") {
+                name = "central"
+                credentials {
+                    this.username = System.getProperty("MAVEN_SONATYPE_USERNAME") ?: System.getenv("MAVEN_SONATYPE_USERNAME")
+                    this.password = System.getProperty("MAVEN_SONATYPE_TOKEN") ?: System.getenv("MAVEN_SONATYPE_TOKEN")
+                }
+            }
             mavenLocal()
         }
     }
@@ -22,7 +30,10 @@ val platformComponentName: String = "javaPlatform"
 val catalogComponentName: String = "versionCatalog"
 
 configure(subprojects.filter { it.name.contains("-bom") }) {
-    apply { plugin("java-platform") }
+    apply {
+        plugin("java-platform")
+        plugin("signing")
+    }
 
     javaPlatform {
         allowDependencies()
@@ -32,19 +43,80 @@ configure(subprojects.filter { it.name.contains("-bom") }) {
         publications {
             create<MavenPublication>("javaPlatform") {
                 from(components[platformComponentName])
+
+                pom {
+                    description = "A set of common dependencies"
+                    url = "https://github.com/zenhelix/zenhelix-dependencies"
+                    licenses {
+                        license {
+                            name = "The Apache License, Version 2.0"
+                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                        }
+                    }
+                    scm {
+                        connection = "scm:git:git://github.com/zenhelix/zenhelix-dependencies.git"
+                        developerConnection = "scm:git:ssh://github.com/zenhelix/zenhelix-dependencies.git"
+                        url = "https://github.com/zenhelix/zenhelix-dependencies"
+                    }
+                }
             }
         }
+    }
+
+    signing {
+        // ORG_GRADLE_PROJECT_signingKeyId
+        val signingKeyId: String? by project
+        // ascii-armored format
+        // ORG_GRADLE_PROJECT_signingKey
+        val signingKey: String? by project
+        // ORG_GRADLE_PROJECT_signingPassword
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+
+        sign(publishing.publications["javaPlatform"])
     }
 }
 
 configure(subprojects.filter { it.name.contains("-toml") }) {
-    apply { plugin("version-catalog") }
+    apply {
+        plugin("version-catalog")
+        plugin("signing")
+    }
 
     publishing {
         publications {
             create<MavenPublication>("versionCatalog") {
                 from(components[catalogComponentName])
+
+                pom {
+                    description = "A set of common dependencies"
+                    url = "https://github.com/zenhelix/zenhelix-dependencies"
+                    licenses {
+                        license {
+                            name = "The Apache License, Version 2.0"
+                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                        }
+                    }
+                    scm {
+                        connection = "scm:git:git://github.com/zenhelix/zenhelix-dependencies.git"
+                        developerConnection = "scm:git:ssh://github.com/zenhelix/zenhelix-dependencies.git"
+                        url = "https://github.com/zenhelix/zenhelix-dependencies"
+                    }
+                }
             }
         }
+    }
+
+    signing {
+        // ORG_GRADLE_PROJECT_signingKeyId
+        val signingKeyId: String? by project
+        // ascii-armored format
+        // ORG_GRADLE_PROJECT_signingKey
+        val signingKey: String? by project
+        // ORG_GRADLE_PROJECT_signingPassword
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+
+        sign(publishing.publications["versionCatalog"])
     }
 }
