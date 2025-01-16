8099ca3dd096dd673cbfbe6ed9c3002f82418100
medakin <dm.medakin.online@gmail.com>
2025-01-16 22:09:47 +0400

    add readme

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,37 +1,81 @@
+@file:Suppress("UnstableApiUsage")
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "zenhelix-dependencies"
+rootProject.name = "dependency-hub"
 
-include(
-    ":multiplatform-client-android-platform:multiplatform-client-android-bom",
-    ":multiplatform-client-android-platform:multiplatform-client-android-toml"
-)
-include(
-    ":multiplatform-client-ios-platform:multiplatform-client-ios-bom",
-    ":multiplatform-client-ios-platform:multiplatform-client-ios-toml"
-)
-include(
-    ":multiplatform-client-web-platform:multiplatform-client-web-bom",
-    ":multiplatform-client-web-platform:multiplatform-client-web-toml"
-)
-include(
-    ":multiplatform-client-desktop-platform:multiplatform-client-desktop-bom",
-    ":multiplatform-client-desktop-platform:multiplatform-client-desktop-toml"
-)
-include(
-    ":server-jvm-platform:server-jvm-bom",
-    ":server-jvm-platform:server-jvm-toml"
-)
+project("multiplatform-client-android-platform") {
+    include("multiplatform-client-android-bom")
+    include("multiplatform-client-android-toml")
+}
 
-include(
-    ":multiplatform-client-platform:multiplatform-client-bom",
-    ":multiplatform-client-platform:multiplatform-client-toml"
-)
-include(
-    ":gradle-plugins-platform:gradle-plugins-bom",
-    ":gradle-plugins-platform:gradle-plugins-toml"
-)
-include(
-    ":gradle-plugins-dev-platform:gradle-plugins-dev-bom",
-    ":gradle-plugins-dev-platform:gradle-plugins-dev-toml"
-)
+project("multiplatform-client-ios-platform") {
+    include("multiplatform-client-ios-bom")
+    include("multiplatform-client-ios-toml")
+}
+
+project("multiplatform-client-web-platform") {
+    include("multiplatform-client-web-bom")
+    include("multiplatform-client-web-toml")
+}
+
+project("multiplatform-client-desktop-platform") {
+    include("multiplatform-client-desktop-bom")
+    include("multiplatform-client-desktop-toml")
+}
+
+project("server-jvm-platform") {
+    include("server-jvm-bom")
+    include("server-jvm-toml")
+}
+
+project("multiplatform-client-platform") {
+    include("multiplatform-client-bom")
+    include("multiplatform-client-toml")
+}
+
+project("gradle-plugins-platform") {
+    include("gradle-plugins-bom")
+    include("gradle-plugins-toml")
+}
+
+project("gradle-plugins-dev-platform") {
+    include("gradle-plugins-dev-bom")
+    include("gradle-plugins-dev-toml")
+}
+
+private fun Settings.project(
+    baseProject: String, initializer: IncludeContext.() -> Unit = {}
+): IncludeContext = IncludeContext(baseProject, this).apply(initializer)
+
+private class IncludeContext(private val baseProject: String, private val delegate: Settings) {
+
+    fun project(
+        baseProject: String, initializer: IncludeContext.() -> Unit = {}
+    ): IncludeContext = IncludeContext("${this.baseProject}:$baseProject", this.delegate).apply(initializer)
+
+    fun include(vararg project: String) {
+        project.forEach {
+            delegate.include("$baseProject:$it")
+        }
+    }
+
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+    }
+
+    plugins {
+        id("io.github.zenhelix.maven-central-publish") version "0.3.0"
+    }
+}
